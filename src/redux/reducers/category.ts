e347db71c0a5eb52835dc4f5a37a12bb6a384462
MariiaD1997 <mariadatskiv@gmail.com>
2023-02-08 14:47:21 +0200

    updated category reducer

--- src/redux/reducers/category.ts
+++ src/redux/reducers/category.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { Category } from "../../types/category";
+import { Category, NewCategory } from "../../types/category";
 
 const initialState: Category[] = [];
 
@@ -12,14 +12,64 @@ export const fetchCategories = createAsyncThunk("fetchCategories", async () => {
   return data;
 });
 
+export const createCategory = createAsyncThunk(
+  "createCategory",
+  async (categoryData: NewCategory) => {
+    const result = await axios.post(
+      "https://api.escuelajs.co/api/v1/categories/",
+      categoryData
+    );
+    const data = result.data;
+    return data;
+  }
+);
+
+export const updateCategory = createAsyncThunk(
+  "updateCategory",
+  async ({ id, categoryData }: { id: number; categoryData: NewCategory }) => {
+    const result = await axios.put(
+      `https://api.escuelajs.co/api/v1/categories/${id}`,
+      categoryData
+    );
+    const data = result.data;
+    return data;
+  }
+);
+
+export const deleteCategory = createAsyncThunk(
+  "deleteCategory",
+  async (id: number) => {
+    const result = await axios.delete(
+      `https://api.escuelajs.co/api/v1/categories/${id}`
+    );
+    const data = result.data;
+    return data;
+  }
+);
+
 const categorySlice = createSlice({
   name: "categorySlicer",
   initialState,
   reducers: {},
   extraReducers: (build) => {
-    build.addCase(fetchCategories.fulfilled, (state, action) => {
-      return action.payload;
-    });
+    build
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(createCategory.fulfilled, (state, action) => {
+        return [...state, action.payload];
+      })
+      .addCase(updateCategory.fulfilled, (state, action) => {
+        return state.map((item) => {
+          if (item.id === action.payload.id) {
+            item = action.payload;
+          }
+          return item;
+        });
+      })
+      .addCase(deleteCategory.fulfilled, (state, action) => {
+        return state;
+      });
   },
 });
 const categoryReducer = categorySlice.reducer;
