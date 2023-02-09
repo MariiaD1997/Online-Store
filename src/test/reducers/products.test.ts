edeb446bf614f16212be4183a2f8e00f3f488421
MariiaD1997 <mariadatskiv@gmail.com>
2023-02-09 09:49:14 +0200

    create products server

--- src/test/reducers/products.test.ts
+++ src/test/reducers/products.test.ts
@@ -7,13 +7,26 @@ import productsReducer, {
   updateOne,
   deleteOne,
 } from "../../redux/reducers/products";
+import productsServer from "../shared/productsServer";
 
 let store = createStore();
 
 beforeEach(() => {
-  categoryServer.listen();
+  productsServer.listen();
 });
 
 afterAll(() => {
-  categoryServer.close();
+  productsServer.close();
+});
+
+describe("test category reducer", () => {
+  //collection of all single test cases
+  test("check initial state", () => {
+    expect(store.getState().productsReducer.length).toBe(0);
+  });
+  test("should return all products from api", async () => {
+    // redux can't work with async funct so we work with the store instead (but it's not normal mostly bcs testing store should be separated)
+    await store.dispatch(fetchProducts());
+    expect(store.getState().categoryReducer.length).toBeGreaterThan(0);
+  });
 });
---
+++ src/test/shared/productsServer.ts
@@ -0,0 +1,69 @@
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { CreateProduct, Product } from "../../types/products";
+
+import testData from "./testData";
+
+export const handler = [
+  // add new product
+  rest.post(
+    "https://api.escuelajs.co/api/v1/products",
+    async (req, res, ctx) => {
+      const product: CreateProduct = await req.json();
+      return res(ctx.json(product));
+    }
+  ),
+
+  //get all products
+  rest.get(
+    "https://api.escuelajs.co/api/v1/products",
+    async (req, res, ctx) => {
+      return res(ctx.json(testData.allProducts));
+    }
+  ),
+  /*
+  // get single product
+  rest.get(
+    "https://api.escuelajs.co/api/v1/products/:id",
+    async (req, res, ctx) => {
+      return res(ctx.json(testData.allProducts));
+    }
+  ),
+*/
+  //update product
+  rest.put(
+    `https://api.escuelajs.co/api/v1/products/:id`,
+    async (req, res, ctx) => {
+      const updateProduct: Product = await req.json();
+      const { id } = req.params;
+      const foundProduct = testData.allProducts.find(
+        (item) => item.id === Number(id)
+      );
+      if (foundProduct) {
+        return res(
+          ctx.json({
+            ...foundProduct,
+            ...updateProduct,
+          })
+        );
+      }
+    }
+  ),
+
+  //delete product
+  rest.delete(
+    `https://api.escuelajs.co/api/v1/products/:id`,
+    async (req, res, ctx) => {
+      const { id } = await req.json();
+      const deletedProduct = testData.allProducts.find(
+        (item) => item.id === Number(id)
+      );
+      if (deletedProduct) {
+        return res(ctx.json(true));
+      }
+    }
+  ),
+];
+
+const productsServer = setupServer(...handler);
+export default productsServer;
